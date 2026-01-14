93a4ef46680993849d08116826c65e6e6ee03022
Raharinda <raharindanadhif@gmail.com>
2026-01-14 22:54:49 +0700

    feat: implement home page role selection UI

---
+++ src/app/(dashboard)/agent/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+import { FiFilter } from 'react-icons/fi'
+import Image from 'next/image'
+import { useState } from 'react'
+import { IoSearchOutline } from 'react-icons/io5'
+
+export default function Agent() {
+    return <div>Agent</div>
+}
